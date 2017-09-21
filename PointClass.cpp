615ee78b782fb9892cf6d545853962aa551a7df3
abigailsturgeon <31452283+abigailsturgeon@users.noreply.github.com>
2017-09-21 13:17:28 -0600

    Initial commit

---
+++ PointClass.cpp
@@ -0,0 +1,44 @@
+// File: PointClass
+// Created by Abigail Sturgeon on 9/21/2017 for CS1410.
+// Copyright (c) 2017 WSU
+
+#include <iostream>
+using namespace std;
+
+// Constants, Structure, Classes
+class Point
+{
+private:
+    float x;
+    float y;
+public:
+    // Constructor
+    Point(): x(0.0), y(0.0) {}
+
+    // Methods
+    void showPoint();
+    {
+        cout << "X = " << x << endl;
+        cout << "Y = " << y << endl;
+    }
+    void updatePoint(float xnew, float ynew);
+    {
+        x = xnew;
+        y = ynew;
+    }
+};
+
+// Main Program Program
+
+int main()
+{
+    Point p1;
+    p1.showPoint();     // Display values
+    p1.updatePoint(2.5, 3.9);       // Update values
+    p1.showPoint();     // Display values
+
+    return 0;
+
+}
+
+// Function Definitions
